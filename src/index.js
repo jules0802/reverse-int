8faa208a9b9c0be23fd6693b4e8dadfd97ce8073
Iuliia Plekhanova <julec2006@yandex.ru>
2020-02-20 14:55:56 +0400

    fix: add solution to index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+
+  let nString = String(Math.abs(n));
+  let result = '';
+  for (let i=nString.length-1; i>=0; i--) {   
+    result+=nString[i];
+  }
+  return parseInt(result.replace(/^0+/,""));
 }
